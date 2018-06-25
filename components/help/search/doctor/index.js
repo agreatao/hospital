c43d4b327d92ba3fdcdc8c87b3f748b72ef87a77
Mr.X <512573042@qq.com>
2018-06-25 19:47:00 +0800

    医生搜索页面接口调试

--- components/help/search/doctor/index.js
+++ components/help/search/doctor/index.js
@@ -1,19 +1,16 @@
-// components/help/search/doctor/index.js
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
-
+    obj: Object
   },
-
   /**
    * 组件的初始数据
    */
   data: {
 
   },
-
+  ready: function() {
+    console.log(this.data.obj);
+  },
   /**
    * 组件的方法列表
    */
