db573bd6ca1da79ff9783da070006bd2579f8569
sysam0627 <1553084133@qq.com>
2018-06-26 20:32:30 +0800

    用户信息全局

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //app.js
 App({
-  onLaunch: function () {
-
-  },
   globalData: {
-    
+    userinfo: null,
+  },
+  onLaunch: function () {
+    wx.request({
+      url: "https://www.dszejt.com/ws/ws_xcx.asmx/GetEjtUserData",
+      data: {
+        strSFZH: "421003198210020013",
+        strKey: "C1BC7666E5C74BD384196-AD1532102C1"
+      },
+      method: "POST",
+      dataType: "JSON",
+      success: (res) => {
+        this.globalData.userinfo = JSON.parse(JSON.parse(res.data).d)[0];
+      }
+    })
   }
 })
--- pages/owner/index.js
+++ pages/owner/index.js
@@ -4,20 +4,11 @@ Page({
     sfz: ''
   },
   onLoad: function (options) {
-    wx.request({
-      url: "https://www.dszejt.com/ws/ws_xcx.asmx/GetEjtUserData",
-      data: {
-        strSFZH: "421003198210020013",
-        strKey: "C1BC7666E5C74BD384196-AD1532102C1"
-      },
-      method: "POST",
-      dataType: "JSON",
-      success: (res) => {
-        this.setData({
-          obj: JSON.parse(JSON.parse(res.data).d)[0],
-          sfz: JSON.parse(JSON.parse(res.data).d)[0].身份证号.slice(0, 6) + "********" + JSON.parse(JSON.parse(res.data).d)[0].身份证号.slice(14, 18)
-        })
-      }
+    var userinfo = getApp().globalData.userinfo; 
+    var sfz = getApp().globalData.sfz; 
+    this.setData({
+      obj: userinfo,
+      sfz: userinfo["身份证号"].slice(0, 6) + "********" + userinfo["身份证号"].slice(14, 18)
     })
   }
 })
