71fb20bf77c18628290e20af9400ae72e527da12
sysam0627 <1553084133@qq.com>
2018-06-26 16:58:36 +0800

    两个充值，报告查询（部分）

--- pages/owner/report/index.js
+++ pages/owner/report/index.js
@@ -1,66 +1,35 @@
-// pages/service/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    navbar: ['病史', '检验', '检查', "医嘱"],
+    currentTab: 4, 
+    list: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  navbarTap: function (e) {
+    var that = this;
+    that.setData({
+      currentTab: e.currentTarget.dataset.idx
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+  onLoad: function (){
+    this.setData({
+      currentTab:1
+    }),
+    wx.request({
+      url: "https://www.dszejt.com/ws/ws_xcx.asmx/V_LAB_TEST_MASTER",
+      data: {
+        strPATIENT_ID: "0000183083",
+        startDate:"2015-01-01",
+        endDate:"2018-01-01",
+        strLisType:2,
+        strKey: "C1BC7666E5C74BD384196-AD1532102C1"
+      },
+      method: "POST",
+      dataType: "JSON",
+      success: (res) => {
+        this.setData({
+          list: JSON.parse(JSON.parse(res.data).d)
+        })
+      }
+    })
+  } 
 })
--- pages/owner/room/index.js
+++ pages/owner/room/index.js
@@ -1,66 +1,33 @@
 // pages/service/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    inputValue: '',
+    clearShow: false,
+    buttonClick:false,
+    disable:true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  listenerInput: function (e) {
+    this.data.inputValue = e.detail.value;
+    if (this.data.inputValue){
+      this.setData({
+        clearShow: true,
+        buttonClick:true,
+        disable: false
+      })
+    }else{
+      this.setData({
+        clearShow: false,
+        buttonClick: false,
+        disable:true 
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  clearInput:function(e){
+    this.setData({
+      inputValue: '',
+      clearShow: false,
+      buttonClick: false,
+      disable: true 
+    });
   }
 })
