e2b303e63caa4e8c288c645c5a75456a8a8a6cb3
sysam0627 <1553084133@qq.com>
2018-06-27 18:52:07 +0800

    修复bug

--- pages/owner/love/index.js
+++ pages/owner/love/index.js
@@ -7,7 +7,7 @@ Page({
     wx.request({
       url: "https://www.dszejt.com/ws/ws_xcx.asmx/Get_XCX_YSZX_GZYSInfo",
       data: {
-        strUserID: "17769",
+        strUserID: getApp().globalData.userinfo['用户ID'],
         strKey: "C1BC7666E5C74BD384196-AD1532102C1"
       },
       method: "POST",
