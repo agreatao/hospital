c6699a1538afe28bea722b41a86d093694691353
Mr.X <512573042@qq.com>
2018-07-06 10:11:54 +0800

    登录接口调试

--- pages/login/index.js
+++ pages/login/index.js
@@ -7,20 +7,20 @@ Page({
     },
 
     // 获取输入账号
-    usernameInput: function (e) {
+    usernameInput: function(e) {
         this.setData({
             cardId: e.detail.value
         })
     },
 
     // 获取输入密码
-    passwordInput: function (e) {
+    passwordInput: function(e) {
         this.setData({
             password: e.detail.value
         })
     },
     // 登录
-    login: function () {
+    login: function() {
         if (this.data.cardId.length == 0) {
             wx.showToast({
                 title: "身份证不能为空",
@@ -32,21 +32,41 @@ Page({
                 icon: "none"
             })
         } else {
-            // wx.request({
-            //   url: "https://www.dszejt.com/ws/ws_xcx.asmx/GetEjtUserLogin",
-            //   data: {
-            //     strSFZH: this.data.cardId,//有数据测试账号"650202192611260017"
-            //     strPwd: this.data.password,
-            //     strKey: "C1BC7666E5C74BD384196-AD1532102C1"
-            //   },
-            //   method: "POST",
-            //   dataType: "JSON",
-            //   success: (res) => {
-            //      app.globalData.userinfo = res;
-            //   }
-            // })
-            wx.switchTab({
-                url: '../appoint/index'
+            wx.request({
+                url: "https://www.dszejt.com/ws/ws_xcx.asmx/GetEjtUserLogin",
+                data: {
+                    strSFZH: this.data.cardId, //有数据测试账号"421003198210020013"
+                    strPwd: this.data.password, // 111111
+                    strKey: "C1BC7666E5C74BD384196-AD1532102C1"
+                },
+                method: "POST",
+                dataType: "JSON",
+                success: (res) => {
+                    console.log(res);
+                    if (JSON.parse(JSON.parse(res.data).d)) {
+                        wx.request({
+                            url: "https://www.dszejt.com/ws/ws_xcx.asmx/GetEjtUserData",
+                            data: {
+                                strSFZH: this.data.cardId,
+                                strKey: "C1BC7666E5C74BD384196-AD1532102C1"
+                            },
+                            method: "POST",
+                            dataType: "JSON",
+                            success: (res) => {
+                                app.globalData.userinfo = JSON.parse(JSON.parse(res.data).d)[0];
+                                wx.switchTab({
+                                    url: '../appoint/index'
+                                })
+                                console.log(app.globalData.userinfo);
+                            }
+                        })
+                    } else {
+                        wx.showToast({
+                            title: "用户名或密码错误",
+                            icon: "none"
+                        })
+                    }
+                }
             })
         }
     }
